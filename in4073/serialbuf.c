6701481ab847f1ae64d14bb8c6cf5c9ad5d5cec7
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2016-09-08 15:25:40 +0200

    Refactored serialbuf to contain all state in a single struct.

--- in4073/serialbuf.c
+++ in4073/serialbuf.c
@@ -1,49 +1,47 @@
 #include "serialbuf.h"
 
-// The buffer for the received data
-unsigned char serialbuf_rx_buffer[SERIALBUF_SIZE];
-// The buffer for the data to be sent
-unsigned char serialbuf_tx_buffer[SERIALBUF_SIZE];
+static int rx_validate_checksum(serialbuf_t* sb);
+static void rx_end(serialbuf_t* sb);
+static void tx_add_chksum(serialbuf_t* sb);
 
-// Callback function when an entire frame is correctly received
-void (*serialbuf_rx_complete_callback)(void) = 0;
-// Callback function when an entire frame is received but there is a checksum error
-void (*serialbuf_rx_error_callback)(void) = 0;
-// The callback function to transmit a single character on the serial line
-//  parameter: unsigned char - the byte to be transmitted on the serial line
-void (*serialbuf_tx_callback)(unsigned char) = 0;
 
-static int rx_validate_checksum(void);
-static void rx_end(void);
-static void tx_add_chksum(void);
-
-int rx_ptr = 0;
+void serialbuf_init(serialbuf_t* sb) {
+    int i;
+    sb->rx_ptr                  = 0;
+    sb->rx_complete_callback    = (void (*)(void)) 0;
+    sb->rx_error_callback       = (void (*)(void)) 0;
+    sb->tx_callback             = (void (*)(unsigned char)) 0;
+    for (i = 0; i < SERIALBUF_SIZE; i++) {
+        sb->rx_buffer[i] = 0;
+        sb->tx_buffer[i] = 0;
+    }
+}
 
 // called when a single character is received on the serial line.
-void serialbuf_receive_char(unsigned char c) {
+void serialbuf_receive_char(serialbuf_t* sb, unsigned char c) {
     int chk_result;
-    serialbuf_rx_buffer[rx_ptr++] = c; // fill buffer
-    if (rx_ptr == SERIALBUF_SIZE) {
-        rx_ptr = 0;
-        rx_end(); // checksum check and callback
+    sb->rx_buffer[rx_ptr++] = c; // fill buffer
+    if (sb->rx_ptr == SERIALBUF_SIZE) {
+        sb->rx_ptr = 0;
+        rx_end(sb); // checksum check and callback
     }
 }
 
 // rx_end: Used to handle when all bytes are received and the buffer is full. Checks checksum and does callbacks.
-void rx_end(void) {
-    if (rx_validate_checksum()) {
-        if (serialbuf_rx_complete_callback)
-            serialbuf_rx_complete_callback();
+void rx_end(serialbuf_t* sb) {
+    if (rx_validate_checksum(sb)) {
+        if (sb->rx_complete_callback)
+            sb->rx_complete_callback();
     } else {
-        if (serialbuf_rx_error_callback)
-            serialbuf_rx_error_callback();
+        if (sb->rx_error_callback)
+            sb->rx_error_callback();
     }
 }
 
 // rx_validate_checksum: Used to check the checksum on the received data.
-// returns 1 if the data and the checksum in the serialbuf_rx_buffer are consistent, otherwise 0
-int rx_validate_chksum(void) {
-    unsigned char chkbuf = chksum(serialbuf_rx_buffer);
+// returns 1 if the data and the checksum in the serialbuf->rx_buffer are consistent, otherwise 0
+int rx_validate_chksum(serialbuf_t* sb) {
+    unsigned char chkbuf = chksum(sb->rx_buffer);
     return (chkbuf == 0);
 }
 
@@ -62,34 +60,34 @@ unsigned char chksum(unsigned char* buf) {
 }
 
 // Reset the receiving line. New data will be interpreted as a new frame.
-void serialbuf_rx_reset(void) {
-    rx_ptr = 0;
+void serialbuf_rx_reset(serialbuf_t* sb) {
+    sb->rx_ptr = 0;
 }
 
 // Send the data loaded into the serialbuf_tx_buffer over the serial line.
 // Calculates checksum and sends data byte-by-byte
-void serialbuf_send(void) {
-    tx_add_chksum();
-    tx_transmit();
+void serialbuf_send(serialbuf_t* sb) {
+    tx_add_chksum(sb);
+    tx_transmit(sb);
 }
 
 // tx_add_chksum: Adds checksum to the 
-void tx_add_chksum(void) {
+void tx_add_chksum(serialbuf_t* sb) {
     unsigned char chkbuf = 0;
     int i;
     // Clear chksum placeholder first
-    serialbuf_tx_buffer[SERIALBUF_CHKSUM_INDEX] &= ~SERIALBUF_CHKSUM_MASK;
+    sb->tx_buffer[SERIALBUF_CHKSUM_INDEX] &= ~SERIALBUF_CHKSUM_MASK;
     // Calculate and add checksum
-    chkbuf = chksum(serialbuf_tx_buffer);
-    serialbuf_tx_buffer[SERIALBUF_CHKSUM_INDEX] |= (SERIALBUF_CHKSUM_MASK & (chkbuf << SERIALBUF_CHKSUM_POS));
+    chkbuf = chksum(sb->tx_buffer);
+    sb->tx_buffer[SERIALBUF_CHKSUM_INDEX] |= (SERIALBUF_CHKSUM_MASK & (chkbuf << SERIALBUF_CHKSUM_POS));
 }
 
-// Calls the serialbuf_tx_callback function on all bytes of the tx buffer to send each byte
-void tx_transmit(void) {
+// Calls the serialbuf->tx_callback function on all bytes of the tx buffer to send each byte
+void tx_transmit(serialbuf_t* sb) {
     int tx_ptr;
-    if (!serialbuf_tx_callback)
+    if (!sb->tx_callback)
         return;
     for (tx_ptr = 0; tx_ptr < SERIALBUF_SIZE; tx_ptr++) {
-        serialbuf_tx_callback(serialbuf_tx_buffer[tx_ptr]);
+        sb->tx_callback(sb->tx_buffer[tx_ptr]);
     }
 }
--- in4073/serialbuf.h
+++ in4073/serialbuf.h
@@ -6,22 +6,32 @@
 #define SERIALBUF_CHKSUM_POS    0
 #define SERIALBUF_CHKSUM_MASK   (0x0F << (SERIALBUF_CHKSUM_POS))
 
-// Ignore this struct for now. It is for later refactoring.
 typedef struct serialbuf {
-    unsigned char* rx_buffer;
-    unsigned char* tx_buffer;
+    // The buffer for the received data
+    unsigned char rx_buffer[SERIALBUF_SIZE];
+    // The buffer for the data to be sent
+    unsigned char tx_buffer[SERIALBUF_SIZE];
+    // Pointer pointing into `rx_buffer` where the next received byte will go
+    int rx_ptr;
+    // Callback function when an entire frame is correctly received
+    void (*rx_complete_callback)(void);
+    // Callback function when an entire frame is received but there is a checksum error
+    void (*rx_error_callback)(void);
+    // The callback function to transmit a single character on the serial line
+    //  parameter: unsigned char - the byte to be transmitted on the serial line
+    void (*tx_callback)(unsigned char);
 } serialbuf_t;
 
-extern unsigned char serialbuf_rx_buffer[SERIALBUF_SIZE];
-extern unsigned char serialbuf_tx_buffer[SERIALBUF_SIZE];
-extern void (*serialbuf_rx_complete_callback)(void);
-extern void (*serialbuf_rx_error_callback)(void);
-extern void (*serialbuf_tx_callback)(unsigned char);
+// Call to initialize the serialbuf structure.
+void serialbuf_init(serialbuf_t* sb);
 
-void serialbuf_receive_char(unsigned char c);
+// Call when a single character is received on the serial line.
+void serialbuf_receive_char(serialbuf_t* sb, unsigned char c);
 
-void serialbuf_rx_reset(void);
+// Call to reset the rx_ptr pointer.
+void serialbuf_rx_reset(serialbuf_t* sb);
 
-void serialbuf_send(void);
+// Call to calculate checksum and send data loaded into the tx_buffer.
+void serialbuf_send(serialbuf_t* sb);
 
 #endif // #ifndef SERIALBUF_H
