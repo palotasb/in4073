ff5d1e98245633d136ceb688d902f9da311504e5
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2016-09-26 22:09:34 +0200

    Added comment describing system equations

--- in4073/mode_2_manual.c
+++ in4073/mode_2_manual.c
@@ -4,27 +4,98 @@
 /** MODE 2 (MANUAL) description
  *  ===========================
  *  
- *          lift          Z          ae1
- *          roll          L          ae2
- *          pitch         M          ae3
- *  +----+  yaw    +---+  N   +---+  ae4  +----+
- *  | PC |-------->| 1 |----->| C |------>| QC |----+
- *  +----+         +---+      +---+       +----+    |
- *                   ^                              | force     (X Y Z)
- *                   |                   X ---------+----->
- *               change to                          V torque    (L M N)
- *               Body frame                    +--------+
- *                                             |1/m*I.dt|
- *             no feedback:                    +--------+
- *             assume same frames                   | velocity  (u v w)
- *             and zero movement         X ---------+----->
- *                                                  V spin      (p q r)
- *                                             +--------+
- *                                             |  I.dt  |<-- Change to Earth frame
- *                                             +--------+
- *                                                  | position  (x y z)
- *                                                  +----->
- *                                       X ---------+ attitude  (phi theta psi)
+ *          lift          Z*          ae1
+ *          roll          L*          ae2
+ *          pitch         M*          ae3
+ *  +----+  yaw    +---+  N*   +---+  ae4  +----+
+ *  | PC |-------->| 1 |------>| C |------>| QC |----+
+ *  +----+         +---+    -  +---+       +----+    |
+ *                   ^                               | force     (X Y Z)
+ *                   |                    X ---------+----->
+ *               change to                           V torque    (L M N)
+ *               Body frame                     +--------+
+ *                                              |1/m*I.dt|
+ *             no feedback:                     +--------+
+ *             assume same frames                    | velocity  (u v w)
+ *             and zero movement          X ---------+----->
+ *                                                   V spin      (p q r)
+ *             * setpoints                      +--------+
+ *                                              |  I.dt  |<-- Change to Earth frame
+ *                                              +--------+
+ *                                                   | position  (x y z)
+ *                                                   +----->
+ *                                        X ---------+ attitude  (phi theta psi)
+ *
+ *
+ *  System equations
+ *  ================
+ *
+ *  Basic equations describing quadcopter dynamics
+ *  ----------------------------------------------
+ *
+ *  +-  -+         +-     -+ +- -+     +-  -+
+ *  | x' |         | t 0 0 | | u |     | x' |
+ *  | y' |     =   | 0 t 0 | | v |   + | y' |
+ *  | z' |         | 0 0 t | | w |     | z' |
+ *  +-  -+[k+1]    +-     -+ +- -+[k]  +-  -+[k]
+ *  Here x', y' and z' are in the body frame so they'd have
+ *  to be transformed to yield meaningful x, y, z values.
+ *  
+ *  +- -+         +-     -+ +- -+     +- -+
+ *  | φ |         | t 0 0 | | p |     | φ |
+ *  | θ |     =   | 0 t 0 | | q |   + | θ |
+ *  | ψ |         | 0 0 t | | r |     | ψ |
+ *  +- -+[k+1]    +-     -+ +- -+[k]  +- -+[k]
+ *  Here I think that the reference frame is the same for
+ *  both Earth and Body and the equation is usable as is.
+ *
+ *  +- -+         +-     -+ +- -+     +- -+
+ *  | u |       1 | t 0 0 | | X |     | u |
+ *  | v |     = - | 0 t 0 | | Y |   + | v |
+ *  | w |       m | 0 0 t | | Z |     | w |
+ *  +- -+[k+1]    +-     -+ +- -+[k]  +- -+[k]
+ *  (m is the weight of the quadcopter)
+ *  
+ *  +- -+         +-     -+ +- -+     +- -+
+ *  | p |       1 | t 0 0 | | L |     | p |
+ *  | q |     = - | 0 t 0 | | M |   + | q |
+ *  | r |       I | 0 0 t | | N |     | r |
+ *  +- -+[k+1]    +-     -+ +- -+[k]  +- -+[k]
+ *  (I is the moment of inertia of the qc)
+ *
+ *                            +-      -+
+ *  +- -+         +-       -+ | ae_1^2 |
+ *  | X |         | 0 0 0 0 | | ae_2^2 |
+ *  | Y |    = -b'| 0 0 0 0 | | ae_3^2 |
+ *  | Z |         | 1 1 1 1 | | ae_4^2 |
+ *  +- -+[k+1]    +-       -+ +-      -+[k]
+ *  (b' is a conversion constant)
+ *
+ *                                   +-      -+
+ *  +- -+         +-              -+ | ae_1^2 |
+ *  | L |         |  0  -b'  0   b'| | ae_2^2 |
+ *  | M |     =   |  b'  0  -b'  0 | | ae_3^2 |
+ *  | N |         | -d'  d' -d'  d'| | ae_4^2 |
+ *  +- -+[k+1]    +-              -+ +-      -+[k]
+ *  (b' and d' are conversion constants)
+ *
+ *
+ *  Relation to sensor readings (to be used in Kalman filter)
+ *  ---------------------------------------------------------
+ *  
+ *  +- -+       +-     -+ +-  -+
+ *  | p |       | 1 0 0 | | sp |
+ *  | q |   =   | 0 1 0 | | sq |
+ *  | r |       | 0 0 1 | | sr |
+ *  +- -+[k]    +-     -+ +-  -+[k]
+ *  
+ *  +- -+       +-     -+ +-   -+
+ *  | φ |       | 0 n 0 | | sax |
+ *  | θ |   =   | n 0 0 | | say |
+ *  | Z |       | 0 0 n | | saz |
+ *  +- -+[k]    +-     -+ +-   -+[k]
+ *  n = 1/m' where m' is a weight constant
+ *  true at small angles where a ~ sin(a)
  *
 **/
 
